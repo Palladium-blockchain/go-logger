acd9d3879308f9d91d92f547f99704360c85692f
Ilya Shulepov <24671528+KernelMrex@users.noreply.github.com>
2026-04-22 23:36:42 +0300

    feat: added Debug and Warn, added object-oriented-style interface. Preparation before next version of the lib API transformation

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -5,6 +5,45 @@ import (
 	"log"
 )
 
+type Logger interface {
+	Debug(msg string, fields ...*Field)
+	Info(msg string, fields ...*Field)
+	Warn(msg string, fields ...*Field)
+	Error(msg string, fields ...*Field)
+}
+
+func New() Logger {
+	return &logger{}
+}
+
+type logger struct {
+}
+
+func (_ *logger) Debug(msg string, fields ...*Field) {
+	Debug(msg, fields...)
+}
+
+func (_ *logger) Info(msg string, fields ...*Field) {
+	Info(msg, fields...)
+}
+
+func (_ *logger) Warn(msg string, fields ...*Field) {
+	Warn(msg, fields...)
+}
+
+func (_ *logger) Error(msg string, fields ...*Field) {
+	Error(msg, fields...)
+}
+
+type LogLevel int
+
+const (
+	DEBUG LogLevel = iota
+	INFO
+	WARNING
+	ERROR
+)
+
 type Field struct {
 	Key   string
 	Value any
@@ -18,15 +57,23 @@ func WithError(err error) *Field {
 	return &Field{Key: "error", Value: err}
 }
 
+func Debug(msg string, fields ...*Field) {
+	l(DEBUG, msg, fields...)
+}
+
 func Info(msg string, fields ...*Field) {
-	l(msg, fields...)
+	l(INFO, msg, fields...)
+}
+
+func Warn(msg string, fields ...*Field) {
+	l(WARNING, msg, fields...)
 }
 
 func Error(msg string, fields ...*Field) {
-	l(msg, fields...)
+	l(ERROR, msg, fields...)
 }
 
-func l(msg string, fields ...*Field) {
+func l(levelLevel LogLevel, msg string, fields ...*Field) {
 	rec := msg
 	for _, field := range fields {
 		rec += " " + field.Key + ": " + fmt.Sprintf("%v", field.Value)
